01ce705072d1b45fbfb23e22330b3c2e470e86d6
Antonio Castro <127856748+antoniocastro11@users.noreply.github.com>
2025-09-27 11:36:14 -0300

    fix: ai training, file reader  (#5)
    * feat: requirements
    
    * feat: config
    
    * feat: ai service
    
    * fix: func name
    
    * feat: flask server
    
    * fix: identation and calling func
    
    * fix: new ai service
    
    * fix: requirements
    
    * feat: preprocessing
    
    * feat: file reader
    
    * fix: extracting text
    
    * fix: ai
    
    * fix: requirements

---
+++ src/backend/utils/fileReader.py
@@ -0,0 +1,27 @@
+# backend/utils/fileReader.py
+import io
+from PyPDF2 import PdfReader
+
+def extract_text_from_file(file):
+    filename = file.filename.lower()
+
+    if filename.endswith(".txt"):
+        text = file.read().decode("utf-8", errors="ignore").strip()
+
+    elif filename.endswith(".pdf"):
+        text = ""
+        try:
+            file.stream.seek(0)
+            pdf_reader = PdfReader(file.stream)
+            for page in pdf_reader.pages:
+                text += page.extract_text() or ""
+        except Exception as e:
+            print(f"Erro ao ler PDF: {e}")
+            return ""
+        text = text.strip()
+        text = " ".join(text.split())
+
+
+    else:
+        raise ValueError("Formato de arquivo não suportado. Use .txt ou .pdf.")
+    return text
